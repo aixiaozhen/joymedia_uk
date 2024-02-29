fe8b3401124bb7a47dee044e49cea179daf1ab51
Aixiaozhen <hi@aixiaozhen.com>
2024-02-29 14:45:10 +0800

    Hidden social account

--- src/App.vue
+++ src/App.vue
@@ -916,12 +916,12 @@ function clickNav() {
             <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
           </div>
         </nav>
-        <div class="mt-10 flex justify-center space-x-10">
+        <!-- <div class="mt-10 flex justify-center space-x-10">
           <a v-for="item in navigation.social" :key="item.name" @click="clickNav" class="text-gray-400 hover:text-gray-500">
             <span class="sr-only">{{ item.name }}</span>
             <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
           </a>
-        </div>
+        </div> -->
         <p class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2024 Joy Media UK Company Ltd All rights reserved.</p>
       </div>
     </footer>
